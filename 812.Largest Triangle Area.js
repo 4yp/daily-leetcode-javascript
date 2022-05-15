ccd5eff13b2a2733cde86adee2cfc90625efd8d2
4yp <myyinpeng@gmail.com>
2022-05-15 22:35:54 +0800

    Largest Triangle Area

---
+++ 812.Largest Triangle Area.js
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=812 lang=javascript
+ *
+ * [812] 最大三角形面积
+ *
+ * https://leetcode-cn.com/problems/largest-triangle-area/description/
+ *
+ * algorithms
+ * Easy (63.24%)
+ * Likes:    158
+ * Dislikes: 0
+ * Total Accepted:    28.9K
+ * Total Submissions: 42.3K
+ * Testcase Example:  '[[0,0],[0,1],[1,0],[0,2],[2,0]]'
+ *
+ * 给定包含多个点的集合，从其中取三个点组成三角形，返回能组成的最大三角形的面积。
+ * 
+ * 
+ * 示例:
+ * 输入: points = [[0,0],[0,1],[1,0],[0,2],[2,0]]
+ * 输出: 2
+ * 解释: 
+ * 这五个点如下图所示。组成的橙色三角形是最大的，面积为2。
+ * 
+ * 
+ * 
+ * 
+ * 注意: 
+ * 
+ * 
+ * 3 <= points.length <= 50.
+ * 不存在重复的点。
+ * -50 <= points[i][j] <= 50.
+ * 结果误差值在 10^-6 以内都认为是正确答案。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var largestTriangleArea = function (points) {
+  const n = points.length
+  let ans = 0.0
+  for (let i = 0; i < n - 2; i++) {
+    for (let j = i + 1; j < n - 1; j++) {
+      for (let k = j + 1; k < n; k++) {
+        [[x1, y1], [x2, y2], [x3, y3]] = [points[i], points[j], points[k]]
+        ans = Math.max(ans, Math.abs(x1 * y2 + x2 * y3 + x3 * y1 - x1 * y3 - x2 * y1 - x3 * y2))
+      }
+    }
+  }
+  return ans / 2
+};
+// @lc code=end
+
