6b2432c35d36d0afe3247b547b27d710a6e1b9f6
4yp <myyinpeng@gmail.com>
2022-05-16 23:12:28 +0800

    Divide Two Integers

---
+++ 29.Divide Two Integers.js
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=29 lang=javascript
+ *
+ * [29] 两数相除
+ *
+ * https://leetcode-cn.com/problems/divide-two-integers/description/
+ *
+ * algorithms
+ * Medium (22.11%)
+ * Likes:    891
+ * Dislikes: 0
+ * Total Accepted:    161K
+ * Total Submissions: 728K
+ * Testcase Example:  '10\n3'
+ *
+ * 给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+ * 
+ * 返回被除数 dividend 除以除数 divisor 得到的商。
+ * 
+ * 整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8 以及 truncate(-2.7335) =
+ * -2
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: dividend = 10, divisor = 3
+ * 输出: 3
+ * 解释: 10/3 = truncate(3.33333..) = truncate(3) = 3
+ * 
+ * 示例 2:
+ * 
+ * 输入: dividend = 7, divisor = -3
+ * 输出: -2
+ * 解释: 7/-3 = truncate(-2.33333..) = -2
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 被除数和除数均为 32 位有符号整数。
+ * 除数不为 0。
+ * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−2^31,  2^31 − 1]。本题中，如果除法结果溢出，则返回 2^31 − 1。
+ * 
+ * 
+ */
+
+// @lc code=start
+// 超时
+var divide = function(a, b) {
+  const INT_MIN = -Math.pow(2, 31)
+  const INT_MAX = Math.pow(2, 31) - 1
+
+  if (a == INT_MIN && b == -1) return INT_MAX
+
+  const sign = (a > 0) ^ (b > 0) ? -1 : 1
+  if (a > 0) a = -a 
+  if (b > 0) b = -b 
+
+  let res = 0
+  while (a <= b) {
+      a -= b
+      res++
+  }
+  return sign == 1 ? res : -res
+};
+// @lc code=end
+
